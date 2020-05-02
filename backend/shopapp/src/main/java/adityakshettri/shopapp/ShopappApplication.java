ef9c372c6732baec085d70beb63ec76e9bd3a3f1
AdityaKshettri <adikshettri1623@gmail.com>
2020-05-02 13:38:06 +0530

    initial commit

---
+++ backend/shopapp/src/main/java/adityakshettri/shopapp/ShopappApplication.java
@@ -0,0 +1,13 @@
+package adityakshettri.shopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopappApplication.class, args);
+	}
+
+}
---
+++ backend/shopapp/src/main/java/adityakshettri/shopapp/config/MyDataRestConfig.java
@@ -0,0 +1,29 @@
+package adityakshettri.shopapp.config;
+
+import adityakshettri.shopapp.entity.Product;
+import adityakshettri.shopapp.entity.ProductCategory;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        
+        HttpMethod[] theUnsupportedActions = {HttpMethod.DELETE, HttpMethod.POST, HttpMethod.PUT};
+        
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+}
